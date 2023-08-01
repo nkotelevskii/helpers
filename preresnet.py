978156f59c54a5ece1e9964a05353775a0158378
Nikita Kotelevskii <niket0996@gmail.com>
2023-08-01 13:15:54 +0300

    Added spectral architecture

--- preresnet.py
+++ preresnet.py
@@ -5,6 +5,7 @@
 
 import torch.nn as nn
 import torchvision.transforms as transforms
+from torch.nn.utils import spectral_norm
 import math
 
 __all__ = ["PreResNet110", "PreResNet56", "PreResNet8", "PreResNet83", "PreResNet164"]
@@ -155,6 +156,80 @@ class PreResNet(nn.Module):
         return x
 
 
+class PreResNetSpectral(nn.Module):
+    def __init__(self, num_classes=10, depth=110):
+        super(PreResNetSpectral, self).__init__()
+        if depth >= 44:
+            assert (depth - 2) % 9 == 0, "depth should be 9n+2"
+            n = (depth - 2) // 9
+            block = Bottleneck
+        else:
+            assert (depth - 2) % 6 == 0, "depth should be 6n+2"
+            n = (depth - 2) // 6
+            block = BasicBlock
+
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 16, n)
+        self.layer2 = self._make_layer(block, 32, n, stride=2)
+        self.layer3 = self._make_layer(block, 64, n, stride=2)
+        self.bn = nn.BatchNorm2d(64 * block.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        def add_sn(m):    
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
+                return spectral_norm(m, n_power_iterations=5)
+            else:
+                return m
+            
+        self.apply(add_sn)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                )
+            )
+
+        layers = list()
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.layer1(x)  # 32x32
+        x = self.layer2(x)  # 16x16
+        x = self.layer3(x)  # 8x8
+        x = self.bn(x)
+        x = self.relu(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
 class PreResNet164:
     base = PreResNet
     args = list()
@@ -266,5 +341,10 @@ class PreResNet8:
 
 class PreResNet20:
     base = PreResNet
+    args = list()
+    kwargs = {"depth": 20}
+
+class PreResNet20Spectral:
+    base = PreResNetSpectral
     args = list()
     kwargs = {"depth": 20}
